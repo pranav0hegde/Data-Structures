572d772bd24daba4304a94e70cbe98ee4a4b97b7
pranav0hegde <155129059+pranav0hegde@users.noreply.github.com>
2024-03-06 18:43:57 +0530

    Add files via upload

---
+++ Stack Implement using linked lists.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+  int data;
+  struct node *next;
+};
+struct node *start=NULL;
+void push();
+void pop();
+void display();
+int main()
+{
+   int val,option;
+    do
+    {
+        printf("\nEnter the number to perform following operations\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
+        scanf("%d",&option);
+        switch(option)
+        {
+            case 1:push();
+            break;
+            case 2:pop();
+            break;
+            case 3:display();
+            break;
+        }
+    }
+    while(option!=4);
+    return 0;
+}
+void push()
+{
+    struct node *new_node;
+    int num;
+    printf("Enter the data\n");
+    scanf("%d",&num);
+    new_node=(struct node*)malloc(sizeof(struct node));
+    new_node->data=num;
+    new_node->next=start;
+    start=new_node;
+}
+void pop()
+{
+  struct node *ptr;
+  ptr=start;
+  if(start==NULL)
+  {
+  printf("Stack is empty\n");
+  exit(0);
+  }
+  else
+  {
+    ptr=start;
+    start=ptr->next;
+    printf("\nElement popped from the stack is: %d\n",ptr->data);
+    free(ptr);
+  }
+
+}
+void display()
+{
+
+    struct node *ptr;
+    ptr=start;
+    while(ptr!=NULL)
+    {
+        printf("\t%d",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("\n");
+}
